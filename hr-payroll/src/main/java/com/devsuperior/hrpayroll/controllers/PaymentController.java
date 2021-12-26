f8d3c07b746dbedf930cafc3978d3007f8777f48
jeff5m <jeffmatheuspereira@gmail.com>
2021-12-26 20:06:50 -0300

    feat: implementing 'hr-payroll' service with mocked worker

---
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/controllers/PaymentController.java
@@ -0,0 +1,28 @@
+package com.devsuperior.hrpayroll.controllers;
+
+import com.devsuperior.hrpayroll.entities.Payment;
+import com.devsuperior.hrpayroll.services.PaymentService;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/payments")
+public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    public PaymentController(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    @GetMapping(value = "/{worker-id}/days/{days}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Payment> getPaymentByWorkerIdAndDays(@PathVariable(name = "worker-id") Long workerId,
+                                                               @PathVariable(name = "days") Integer days) {
+        return ResponseEntity.ok(paymentService.getPayment(workerId, days));
+    }
+
+}
