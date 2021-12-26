f8dcc4ac94d2b855155a0520692a560efecaf3d6
jeff5m <jeffmatheuspereira@gmail.com>
2021-12-26 20:35:15 -0300

    feat: implementing 'hr-payroll' service with RestTemplate

---
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/entities/Worker.java
@@ -0,0 +1,68 @@
+package com.devsuperior.hrpayroll.entities;
+
+import java.io.Serializable;
+
+@SuppressWarnings("unused")
+public class Worker implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String name;
+    private Double dailyIncome;
+
+    /**
+     * @deprecated - Constructor used by Jackson
+     */
+    @Deprecated
+    public Worker() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getDailyIncome() {
+        return dailyIncome;
+    }
+
+    public void setDailyIncome(Double dailyIncome) {
+        this.dailyIncome = dailyIncome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Worker worker = (Worker) o;
+
+        return id.equals(worker.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "Worker{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", dailyIncome=" + dailyIncome +
+                '}';
+    }
+}
--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
@@ -1,13 +1,29 @@
 package com.devsuperior.hrpayroll.services;
 
 import com.devsuperior.hrpayroll.entities.Payment;
+import com.devsuperior.hrpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
+    private final RestTemplate restTemplate;
+    private final String workerServiceUrl;
+
+    public PaymentService(RestTemplate restTemplate,
+                          @Value("${hr-worker.host}") String workerServiceUrl) {
+        this.restTemplate = restTemplate;
+        this.workerServiceUrl = workerServiceUrl;
+    }
+
     public Payment getPayment(Long workerId, Integer days) {
-        return new Payment("Worker Mock", 200D, days);
+        Map<String, String> uriVariables = Map.of("id", workerId.toString());
+        Worker worker = restTemplate.getForObject(workerServiceUrl + "/{id}", Worker.class, uriVariables);
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 
 }
