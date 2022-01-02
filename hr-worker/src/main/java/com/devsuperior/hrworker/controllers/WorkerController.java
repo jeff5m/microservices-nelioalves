459af2cbbee51a1673c21814931d147aa089483f
jeff5m <jeff.matheus18@gmail.com>
2022-01-02 19:29:09 -0300

    feat: implementing actuator in 'hr-worker' for runtime config refresh

--- hr-worker/src/main/java/com/devsuperior/hrworker/controllers/WorkerController.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/controllers/WorkerController.java
@@ -12,10 +12,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.annotation.RequestScope;
 
 import java.util.List;
 import java.util.Optional;
 
+@RequestScope
 @RestController
 @RequestMapping("/workers")
 public class WorkerController {
