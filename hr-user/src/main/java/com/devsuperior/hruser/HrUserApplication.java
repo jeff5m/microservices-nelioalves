8a02e573f1d0fed42836f2bb3cdddafcc6a914aa
jeff5m <jeff.matheus18@gmail.com>
2022-01-03 22:25:36 -0300

    feat: add data.sql and logging bcrypt hash

--- hr-user/src/main/java/com/devsuperior/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/devsuperior/hruser/HrUserApplication.java
@@ -1,15 +1,34 @@
 package com.devsuperior.hruser;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class HrUserApplication {
+public class HrUserApplication implements CommandLineRunner {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(HrUserApplication.class);
+
+    private final BCryptPasswordEncoder bCrypt;
+
+    public HrUserApplication(BCryptPasswordEncoder bCrypt) {
+        this.bCrypt = bCrypt;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(HrUserApplication.class, args);
+    }
+
+
+    @Override
+    public void run(String... args) {
+        String encodedPassword = bCrypt.encode("123456");
+        LOGGER.info("BCRYPT = {}", encodedPassword);
     }
 
 }
